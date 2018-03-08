2ed4ac24a1b65c872154d732216635203cdd1d12
125Pavithra <125pavithra@gmail.com>
2018-03-08 17:05:53 +0530

    Initial

---
+++ GitProject/Controllers/DefaultController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GitProject.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        public string Post()
+        {
+            return "Post the data";
+        }
+    }
+}
